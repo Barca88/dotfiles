bd80989cc8a36fededb458f5cd8f73c02d65eaad
Marco Gonçalves <marcomatiaspg@gmail.com>
2018-10-28 19:13:27 +0000

    :wave: :speedboat: :wave:

--- install.sh
+++ install.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env sh
 
-. packages.sh
+#. packages.sh
 
-echo "Updating packages..."
-sudo pacman -Syu --needed --noconfirm
+#echo "Updating packages..."
+#sudo pacman -Syu --needed --noconfirm
 
-for pkg in "${PKG[@]}"
-do
-  echo "Installing ${pkg}..."
-  sudo pacman -Syu "$pkg" --needed --noconfirm
-done
+#for pkg in "${PKG[@]}"
+#do
+#  echo "Installing ${pkg}..."
+#  sudo pacman -Syu "$pkg" --needed --noconfirm
+#done
 
-for aur in "${AUR[@]}"
-do
-  echo "Installing ${aur}..."
-  sudo yaourt -Syu "$aur" --needed --noconfirm
-done
+#for aur in "${AUR[@]}"
+#do
+#  echo "Installing ${aur}..."
+#  sudo yaourt -Syu "$aur" --needed --noconfirm
+#done
 
 SAVED_DIR=$PWD
 
@@ -27,9 +27,6 @@ do
   echo "Installing ${dir}..."
   sh "$dir"/install.sh
 done
-
-sudo curl -o /usr/share/backgrounds/wallpaper.jpg  "https://images.unsplash.com/photo-1533759233673-2d30c9bc1e93"
-sudo curl -o /usr/share/backgrounds/login.jpg      "https://images.unsplash.com/photo-1521080755838-d2311117f767"
 
 cd "$SAVED_DIR"
 
