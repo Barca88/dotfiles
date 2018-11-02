61bf49654e3e897246b3f515d4b684eead9b6e88
Marco Gonçalves <marcomatiaspg@gmail.com>
2018-11-02 02:51:32 +0000

    LOL!

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,9 +1,8 @@
 # ALIASES
 
+alias al="cd ~/.dotfiles/zsh/aliases"
 alias dot="cd $HOME/.dotfiles && nvim ."
 alias spl="cd $HOME/.spells && nvim ."
-
-alias s="cd $HOME/Snippets && ls -l"
 
 alias um="cd $HOME/Code/uminho/"
 
@@ -14,13 +13,13 @@ alias ka="killall"
 alias :q="exit"
 
 alias massena="date -d '+1 hour' '+%T' && weather Brussels Belgium 0"
-alias eye="sudo redshift"
+
 alias miei="slack-term --config $HOME/.slack-miei"
 alias cesium="xrandr --output HDMI1 --auto --above eDP1"
 alias li1="cd ~/Universidade/LI1/2018li1g142/src/ && svn update"
 alias li1..="cd ~/Universidade/LI1/2018li1g142/src/" 
 alias um="cd ~/Universidade/"
-alias bsc="git add --all && git commit -m ':wave: :speedboat: :wave:' && git push"
+alias bsc="git add --all && git commit -m \$(curl -sk https://whatthecommit.com/index.txt) && git push"
 alias c="clear"
 alias p="cd ~/Projects/"
 alias sd="cd ~/Universidade/SD/"
@@ -28,3 +27,9 @@ alias sd="cd ~/Universidade/SD/"
 alias open="okular"
 alias atomic="cd ~/Projects/atomic/"
 alias sensors="watch -n 1 -d sensors"
+
+alias tempoBraga="curl -H \"Accept-Language: pt\" http://wttr.in/~Braga"
+alias cheat="curl cheat.sh/latency"
+#Awsomeness
+alias map="telnet mapscii.me"
+
