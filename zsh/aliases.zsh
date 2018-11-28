e7da444d209f600fbd86fbdd7a8a5f1816ee3c51
Marco Gonçalves <marcomatiaspg@gmail.com>
2018-11-28 07:17:14 +0000

    bad things happen when you forget about that one little change you made ages ago

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,35 +1,32 @@
 # ALIASES
 
-alias al="cd ~/.dotfiles/zsh/aliases"
-alias dot="cd $HOME/.dotfiles && nvim ."
-alias spl="cd $HOME/.spells && nvim ."
+alias al="nvim ~/.dotfiles/zsh/aliases"
+alias dot="cd $HOME/.dotfiles"
 
 alias um="cd $HOME/Code/uminho/"
 
 alias v="nvim"
 alias vim="nvim"
-alias r="ranger"
 alias ka="killall"
 alias :q="exit"
 
-alias massena="date -d '+1 hour' '+%T' && weather Brussels Belgium 0"
 
-alias miei="slack-term --config $HOME/.slack-miei"
-alias cesium="xrandr --output HDMI1 --auto --above eDP1"
-alias li1="cd ~/Universidade/LI1/2018li1g142/src/ && svn update"
-alias li1..="cd ~/Universidade/LI1/2018li1g142/src/" 
+alias li1="cd ~/Projects/2018li1g142/src && svn update"
+alias li1..="cd ~/Projects/2018li1g142/src/" 
 alias um="cd ~/Universidade/"
 alias bsc="git add --all && git commit -m '$(curl -sk https://whatthecommit.com/index.txt)' && git push"
 alias c="clear"
 alias p="cd ~/Projects/"
 alias sd="cd ~/Universidade/SD/"
+
 #PDF 
 alias open="okular"
 alias atomic="cd ~/Projects/atomic/"
 alias sensors="watch -n 1 -d sensors"
 
 alias tempoBraga="curl -H \"Accept-Language: pt\" http://wttr.in/Braga"
-alias cheat="curl cheat.sh/latency"
-#Awsomeness
+
+#Awesomeness
 alias map="telnet mapscii.me"
+alias cheat="curl cheat.sh/latency"
 
