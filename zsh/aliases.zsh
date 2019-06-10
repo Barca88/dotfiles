d70ddcdf202555d025b471b8860a96039e85e38b
Marco Gonçalves <marcomatiaspg@gmail.com>
2019-06-10 17:28:45 +0100

    giggle.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -12,17 +12,23 @@ alias um="cd ~/Universidade/"
 alias bsc='git add --all && git commit -m "$(curl -sk https://whatthecommit.com/index.txt)" && git push'
 alias c="clear"
 alias p="cd ~/Projects/"
-alias sd="cd ~/Universidade/SD/"
+
+#Makefile
+alias clean="make clean"
+alias run="make run"
 
 #PDF 
 alias open="okular"
 alias atomic="cd ~/Projects/atomic/"
+#Sensores
 alias sensors="watch -n 1 -d sensors"
-
+#Tempo
 alias tempoBraga="curl -H \"Accept-Language: pt\" http://wttr.in/Braga"
 alias Barca="curl -H \"Accept-Language: pt\" http://wttr.in/Ponte da Barca"
-
 #Awesomeness
 alias map="telnet mapscii.me"
 alias cheat="curl cheat.sh/latency"
 alias specs="screenfetch"
+
+#Temporary
+alias so="cd ~/Projects/so18-19"
